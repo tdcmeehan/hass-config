835b1f4971497341500cbcbdf33c5379adba3622
Tim Meehan <tim@timdmeehan.com>
2017-01-29 13:49:18 -0500

    Delete blink.py

--- custom_components/blink.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-import voluptuous as vol
-
-from homeassistant.components.light import ATTR_BRIGHTNESS, Light, PLATFORM_SCHEMA
-from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
-import homeassistant.helpers.config_validation as cv
-
-
