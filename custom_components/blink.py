bc30679d93063609ea8518950fee13e8f6c1e1ea
tdcmeehan <tdcmeehan@gmail.com>
2017-01-29 12:27:19 -0500

    automation

---
+++ custom_components/blink.py
@@ -0,0 +1,9 @@
+import logging
+
+import voluptuous as vol
+
+from homeassistant.components.light import ATTR_BRIGHTNESS, Light, PLATFORM_SCHEMA
+from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
+import homeassistant.helpers.config_validation as cv
+
+
